2c75c293e2a8f63d0475165f06328a47c0a15369
Rasula Caldera <rasula.caldera@hotmail.com>
2024-07-19 16:32:25 +0200

    removing email context

--- src/main/java/com/bootsandcodes/fashiondealfinder/NotifyDeals.java
+++ src/main/java/com/bootsandcodes/fashiondealfinder/NotifyDeals.java
@@ -1,7 +1,7 @@
 package com.bootsandcodes.fashiondealfinder;
 
 import com.bootsandcodes.fashiondealfinder.dealfinder.DealFinderContext;
-import com.bootsandcodes.fashiondealfinder.publisher.PublisherContext;
+import com.bootsandcodes.fashiondealfinder.publisher.EmailPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,15 +9,15 @@ import org.springframework.stereotype.Component;
 public class NotifyDeals {
 
     private final DealFinderContext dealFinderContext;
-    private final PublisherContext publisherContext;
+    private final EmailPublisher emailPublisher;
 
-    public NotifyDeals(DealFinderContext dealFinderContext, PublisherContext publisherContext) {
+    public NotifyDeals(DealFinderContext dealFinderContext, EmailPublisher emailPublisher) {
         this.dealFinderContext = dealFinderContext;
-        this.publisherContext = publisherContext;
+        this.emailPublisher = emailPublisher;
     }
 
     @Scheduled(initialDelay = 0, fixedRate = 60000)
     void notifyDeals() {
-        publisherContext.publishDeals(dealFinderContext.findDeals());
+        emailPublisher.publish(dealFinderContext.findDeals());
     }
 }
