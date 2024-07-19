3964c246cf2316e10ec811bfd3778d78f99dd2dc
Rasula Caldera <rasula.caldera@hotmail.com>
2024-07-19 12:35:06 +0200

    email sent

--- src/main/java/com/bootsandcodes/fashiondealfinder/publisher/EmailPublisher.java
+++ src/main/java/com/bootsandcodes/fashiondealfinder/publisher/EmailPublisher.java
@@ -1,10 +1,26 @@
 package com.bootsandcodes.fashiondealfinder.publisher;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailPublisher implements Publish {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
     @Override
     public void publish() {
+        sendSimpleMessage("rasula.caldera@gmail.com", "Test Subject", "Test email body");
+    }
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
     }
 }
