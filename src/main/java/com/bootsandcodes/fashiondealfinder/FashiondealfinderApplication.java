adb533db089bb2c3f486d9284f5390144644f245
Rasula Caldera <rasula.caldera@hotmail.com>
2024-07-19 09:25:33 +0200

    initial commit

---
+++ src/main/java/com/bootsandcodes/fashiondealfinder/FashiondealfinderApplication.java
@@ -0,0 +1,13 @@
+package com.bootsandcodes.fashiondealfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashiondealfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashiondealfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootsandcodes/fashiondealfinder/FashiondealfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootsandcodes.fashiondealfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashiondealfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
